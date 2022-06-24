b6fef182e152dcf0c354803ccbfaaa12022046d1
Caleb Phillips <csphillips5@gmail.com>
2022-06-24 16:05:43 -0400

    implement react-router-dom

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './components/App';
+import Landing from './pages/Landing';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={ <Landing /> } />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
