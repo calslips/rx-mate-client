d34a25455800699b6f43d5d0e82979f7f850c2b2
Caleb Phillips <csphillips5@gmail.com>
2022-06-26 19:23:14 -0400

    initialize Landing page to contain Login/Registration components

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import Login from '../components/entry/Login';
+import Registration from '../components/entry/Registration';
 
 const Landing = () => {
+  const [isNewUser, setIsNewUser] = React.useState<Boolean>(false);
   return (
-    <div>
-      Landing
+    <div className='flex w-full h-screen'>
+      <div className='w-1/3 bg-cyan-400'></div>
+      <div className='w-2/3 relative max-w-xs mx-auto'>
+        <div className='absolute inset-0 m-auto' style={{height: '300px'}}>
+          {( isNewUser && <Registration renderLogin={() => setIsNewUser(false)} /> ) || <Login renderRegistration={() => setIsNewUser(true)} />}
+        </div>
+      </div>
     </div>
   )
 }
