5466215a99fb0734e73b7ffeeadc491587975139
Caleb Phillips <csphillips5@gmail.com>
2022-06-30 22:56:46 -0400

    implement get req of user data, store in local state, display username

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,10 +1,31 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Navbar from '../components/Navbar';
 
 const Dashboard = () => {
+  const [user, setUser] = useState('');
+
+  useEffect(() => {
+    (async function getUserData() {
+      try {
+        const result = await axios.get('/user', {
+          headers: {
+            token: localStorage.getItem('token') || ''
+          }
+        })
+        console.log(result);
+        setUser(result.data.user.username);
+      }
+      catch (err) {
+        console.error(err);
+      }
+    })();
+  }, []);
+
   return (
     <>
       <Navbar />
-      <div>Dashboard</div>
+      <div>{user}'s Dashboard</div>
     </>
   )
 }
