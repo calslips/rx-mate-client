35b5b60a4b83d4918276f378be3043f0d1fe89f2
Caleb Phillips <csphillips5@gmail.com>
2022-07-05 20:58:57 -0400

    retrieve users med data on load, render med form/list

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,20 +1,28 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import MedicationForm from '../components/medication/MedicationForm';
+import MedicationList, { Medication } from '../components/medication/MedicationList';
 import Navbar from '../components/Navbar';
 
 const Dashboard = () => {
-  const [user, setUser] = useState('');
+  const [user, setUser] = useState<string>('');
+  const [medications, setMedications] = useState<Medication[]>([]);
 
   useEffect(() => {
     (async function getUserData() {
       try {
-        const result = await axios.get('/user', {
+        const userRes = await axios.get('/user', {
           headers: {
             token: localStorage.getItem('token') || ''
           }
         })
-        console.log(result);
-        setUser(result.data.user.username);
+        const medRes = await axios.get('/medications', {
+          headers: {
+            token: localStorage.getItem('token') || ''
+          }
+        })
+        setUser(userRes.data.user.username);
+        setMedications(medRes.data.medications);
       }
       catch (err) {
         console.error(err);
@@ -25,7 +33,11 @@ const Dashboard = () => {
   return (
     <>
       <Navbar />
-      <div>{user}'s Dashboard</div>
+      <section className='max-w-md mx-auto p-8'>
+        <h1 className='font-bold mb-8 text-cyan-400 text-center text-xl'>{user}'s Medications</h1>
+        <MedicationForm meds={medications} setMeds={setMedications} />
+        <MedicationList meds={medications} setMeds={setMedications} />
+      </section>
     </>
   )
 }
