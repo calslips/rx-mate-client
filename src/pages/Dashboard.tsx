109b701300446c9c45170da9c2aedb7def4f1b87
Caleb Phillips <csphillips5@gmail.com>
2022-07-10 18:54:05 -0400

    extract & export getUserData function, remove MedicationForm component

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,31 +1,33 @@
 import axios from 'axios';
 import { useEffect } from 'react';
-import MedicationForm from '../components/medication/MedicationForm';
+import { Medication } from '../components/medication/MasterMedList';
 import MedicationList from '../components/medication/MedicationList';
 import Navbar from '../components/Navbar';
 import { UserDataProps } from '../App';
 
+export const getUserData = async (saveUser: (user: string) => void, saveMeds: (meds: Medication[]) => void) => {
+  try {
+    const userRes = await axios.get('/user', {
+      headers: {
+        token: localStorage.getItem('token') || ''
+      }
+    });
+    const medRes = await axios.get('/medications', {
+      headers: {
+        token: localStorage.getItem('token') || ''
+      }
+    });
+    saveUser(userRes.data.user.username);
+    saveMeds(medRes.data.medications);
+  }
+  catch (err) {
+    console.error(err);
+  }
+}
+
 const Dashboard = ({ user, setUser, meds, setMeds }: UserDataProps) => {
   useEffect(() => {
-    (async function getUserData() {
-      try {
-        const userRes = await axios.get('/user', {
-          headers: {
-            token: localStorage.getItem('token') || ''
-          }
-        })
-        const medRes = await axios.get('/medications', {
-          headers: {
-            token: localStorage.getItem('token') || ''
-          }
-        })
-        setUser(userRes.data.user.username);
-        setMeds(medRes.data.medications);
-      }
-      catch (err) {
-        console.error(err);
-      }
-    })();
+    getUserData(setUser, setMeds);
   }, [setUser, setMeds]);
 
   return (
@@ -33,7 +35,6 @@ const Dashboard = ({ user, setUser, meds, setMeds }: UserDataProps) => {
       <Navbar />
       <section className='max-w-md mx-auto p-8'>
         <h1 className='font-bold mb-8 text-cyan-400 text-center text-xl'>{user}'s Medications</h1>
-        <MedicationForm meds={meds} setMeds={setMeds} />
         <MedicationList meds={meds} setMeds={setMeds} />
       </section>
     </>
