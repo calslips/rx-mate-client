64932a2b224d94d58685cf6e5abb354ee6f399fd
Caleb Phillips <csphillips5@gmail.com>
2022-07-10 18:42:31 -0400

    implement component to initiate req to add/remove medication from db

---
+++ src/components/medication/MasterMedList.tsx
@@ -0,0 +1,51 @@
+import axios from 'axios';
+
+export interface Medication {
+  _id: string;
+  name: string;
+  administered: boolean;
+}
+
+export interface MasterMedListProps {
+  meds: Medication[];
+  setMeds: (meds: Medication[]) => void;
+}
+
+const MasterMedList = ({ meds, setMeds }: MasterMedListProps) => {
+  const removeFromList = async (medication: Medication) => {
+    try {
+    const res = await axios.delete(`/medication/${medication._id}`, {
+      headers: {
+        token: localStorage.getItem('token') || '',
+      }
+    });
+    if (res.status === 204) {
+      const newList = meds.filter(med => med._id !== medication._id);
+      setMeds(newList);
+    }
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <ul>
+      {meds.map(med => (
+        <li
+          className='border border-slate-400 flex items-center justify-between mb-2 p-4 rounded-md'
+          key={med._id}
+        >
+          {med.name}
+          <input
+            className='bg-red-400 cursor-pointer hover:text-white px-4 py-2 rounded-md'
+            type='button'
+            value='Remove'
+            onClick={() => {removeFromList(med)}}
+          />
+        </li>
+      ))}
+    </ul>
+  )
+};
+
+export default MasterMedList;
