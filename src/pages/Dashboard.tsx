dc964c1effd6406e70a825178d01e79b025f853c
Caleb Phillips <csphillips5@gmail.com>
2022-07-11 20:37:35 -0400

    modify getUserData() to match new data schema

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,24 +1,18 @@
 import axios from 'axios';
 import { useEffect } from 'react';
-import { Medication } from '../components/medication/MasterMedList';
+import { Medication, UserDataProps } from '../types';
 import MedicationList from '../components/medication/MedicationList';
 import Navbar from '../components/Navbar';
-import { UserDataProps } from '../App';
 
 export const getUserData = async (saveUser: (user: string) => void, saveMeds: (meds: Medication[]) => void) => {
   try {
-    const userRes = await axios.get('/user', {
+    const res = await axios.get('/user', {
       headers: {
         token: localStorage.getItem('token') || ''
       }
     });
-    const medRes = await axios.get('/medications', {
-      headers: {
-        token: localStorage.getItem('token') || ''
-      }
-    });
-    saveUser(userRes.data.user.username);
-    saveMeds(medRes.data.medications);
+    saveUser(res.data.user.username);
+    saveMeds(res.data.user.medications);
   }
   catch (err) {
     console.error(err);
