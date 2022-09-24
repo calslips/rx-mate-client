dd9fc6c57f23ef4b4876446a59ec3a76534810c8
Caleb Phillips <csphillips5@gmail.com>
2022-09-23 21:58:14 -0400

    refactor push event listener to show received notifications

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -2,6 +2,15 @@ this.addEventListener('activate', event => {
   console.log('Service worker has been activated');
 });
 
-this.addEventListener('push', event => {
-  console.log('Notifications will be displayed here');
+this.addEventListener('push', async event => {
+  const message = await event.data.json();
+  const { title, description, image } = message;
+  console.log({ message });
+  console.log({ event })
+  await event.waitUntil(
+    this.registration.showNotification(title, {
+      body: description,
+      icon: image,
+    })
+  );
 });
