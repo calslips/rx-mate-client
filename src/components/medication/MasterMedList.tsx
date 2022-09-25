d4f3c3ebfc527f02e2e9b79964717a865811afb3
Caleb Phillips <csphillips5@gmail.com>
2022-09-24 20:31:26 -0400

    add ability to filter master med list by med name

--- src/components/medication/MasterMedList.tsx
+++ src/components/medication/MasterMedList.tsx
@@ -1,7 +1,10 @@
 import axios from 'axios';
+import { useRef } from 'react';
 import { MasterMedListProps, Medication } from '../../types';
 
 const MasterMedList = ({ meds, setMeds }: MasterMedListProps) => {
+  const sortedMeds = [...meds].sort((a, b) => a.name.localeCompare(b.name));
+  const medsRef = useRef<HTMLLIElement[] | null[]>([]);
   const removeFromList = async (medication: Medication) => {
     try {
       const res = await axios.delete(`/medication/${medication._id}`, {
@@ -17,14 +20,32 @@ const MasterMedList = ({ meds, setMeds }: MasterMedListProps) => {
       console.error(err);
     }
   };
+  const filterList = (e: React.ChangeEvent<HTMLInputElement>) => {
+    medsRef.current.map((ref, i) =>
+      sortedMeds[i]?.name.toLowerCase().includes(e.target.value.toLowerCase())
+        ? (ref?.classList.remove('hidden'))
+        : (ref?.classList.add('hidden'))
+    );
+  };
 
   return (
     <section className='max-w-lg'>
       <h2 className='font-bold mb-10 text-center text-sky-500'>Medication List</h2>
+      <div className='flex items-center justify-between pb-2'>
+        <label className='font-bold' htmlFor="filter">Filter By Medication Name:</label>
+        <input
+          className='border border-sky-500 px-4 py-2 rounded-md w-1/2'
+          id='filter'
+          name='filter'
+          type='text'
+          onChange={e => {filterList(e)}}
+        />
+      </div>
       <ul>
-        {[...meds].sort((a, b) => a.name.localeCompare(b.name)).map(med => (
+        {sortedMeds.map((med, i) => (
           <li
             className='border border-slate-400 flex items-center justify-between mb-2 p-4 rounded-md'
+            ref={e => medsRef.current[i] = e}
             key={med._id}
           >
             {med.time} {med.count} {med.name} {med.type} {med.dose}
